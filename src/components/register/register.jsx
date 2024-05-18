698bdc7877ae4c6ac8fbbe068a346f041f7e50dc
Denis Andronov <denisunderonov@gmail.com>
2024-05-18 23:07:57 +0300

    realize authorization

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,12 +1,44 @@
 import Header from "../header/header";
 import "./register.css";
+
+const url = "http://localhost:4445/auth/register";
+
 export default function RegisterPage() {
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const formObject = {};
+    formData.forEach((value, key) => {
+      formObject[key] = value;
+    });
+    console.log(formObject);
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(formObject),
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Ошибка сети: ${response.message}`);
+      }
+
+      const data = await response.json();
+      console.log("Успешная регистрация:", data);
+    } catch (error) {
+      console.error("Ошибка при отправке данных:", error);
+    }
+  }
+
   return (
     <>
       <Header />
       <div className="registration">
         <div className="registration-container">
-          <form id="registration-form">
+          <form id="registration-form" onSubmit={(e) => handleSubmit(e)}>
             <p className="registration-form__account-text">регистрация</p>
             <input type="text" name="login" placeholder="Введите логин" />
             <input type="email" name="email" placeholder="Введите почту" />
@@ -15,7 +47,10 @@ export default function RegisterPage() {
               name="password"
               placeholder="Введите пароль"
             />
-            <button className="registration-form__register-button btn btn-primary">
+            <button
+              type="submit"
+              className="registration-form__register-button btn btn-primary"
+            >
               Зарегистрироваться
             </button>
           </form>
